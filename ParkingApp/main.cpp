fa1361cbdd95cffce413a1ec3bfa837a5c0cf8ab
Maxim Tytskiy <maxim.tytskiy@collabio.team>
2021-01-16 17:02:40 +0200

    Initial Commit

---
+++ ParkingApp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ParkingApp
+//
+//  Created by Maxim Tytskiy on 16.01.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
