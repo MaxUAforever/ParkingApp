3e8ab5ec5cc229f5799a7d536e844691759702fe
Maxim Tytskiy <maxim.tytskiy@collabio.team>
2021-02-10 01:59:50 +0200

    Dependencies were refactored

--- ParkingApp/Externals/TimeManager.cpp
+++ ParkingApp/Externals/TimeManager.cpp
@@ -1,13 +1,40 @@
 #include "TimeManager.hpp"
 
+#include <cstdlib>
+#include <iostream>
+
 namespace ParkingEngine
 {
 
-static size_t currentTime = 0;
-
-size_t TimeManager::getCurrentTime()
+void TimeManager::startSession(EntryKeyID keyID)
 {
-    return ++currentTime;
+    _sessionStartTimes.emplace(keyID, getCurrentTime());
+}
+
+void TimeManager::stopSession(EntryKeyID keyID)
+{
+    _sessionStartTimes.erase(keyID);
+}
+
+TimeType TimeManager::getSessionDuraton(EntryKeyID keyID) const
+{
+    const auto startTimeIt = _sessionStartTimes.find(keyID);
+    if (startTimeIt == _sessionStartTimes.end())
+    {
+        return 0;
+    }
+    
+    const auto& startTime = startTimeIt->second;
+    
+    return getCurrentTime() - startTime;
+}
+
+size_t TimeManager::getCurrentTime() const
+{
+    // TODO: remove this code from function
+    static size_t currentTime = 0;
+    
+    return currentTime++;
 }
 
 } // namespace ParkingEngine
